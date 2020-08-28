92e91e87302d5b5285668d39621952303dc2d0fb
Alessandro <alessandro.boscato@gmail.com>
2020-08-28 16:44:31 +0200

    2° tentativo (errore)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 //1. Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
-//Creo la funzione random e stampo in console in numero
-var numberList = [""];
+// var bombe = 16;
+// var tentativi = 0;
+// Creo la funzione random e stampo in console il numero
+var numberListCPU = [];
 function randomN(nmin, nmax) {
   var randomNumber = Math.floor(Math.random() * nmax + nmin);
   return randomNumber;
 }
-
 var randomNumber = randomN(1, 100);
 console.log(randomNumber);
 
-// Aggiungo il numero nell'array se non è già presente
+// funzione che dato un array ed un numero controlla che il numero non sia già presente nell'array
+function check(array, num) {
+  var output = true;
+  if (array.includes(num)) {
+    return output = true;
+  } else {
+    return output = false;
+  }
+}
+var checkNumCPU = check(numberListCPU, randomNumber);
+console.log(checkNumCPU);
+
+//se non è prente, lo inserisco nell'array
+
 var i = 0;
 while (i < 10) {
   randomNumber;
-  if (numberList.includes(randomNumber[i])) {
-    randomNumber;
-  } else {
-    numberList.push(randomNumber[i]);
+  if (checkNumCPU == false) {
+    numberListCPU.push(randomNumber);
+    console.log(numberListCPU);
   }
-  console.log(numberList);
+  randomNumber;
   i++
 }
 
-numberList.length != 16
+// numberList.length != 16
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
