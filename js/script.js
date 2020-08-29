29a9f3b4eaf6e6dbc00fe9a5ba2ebf51658105d8
Alessandro <alessandro.boscato@gmail.com>
2020-08-29 18:23:33 +0200

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -14,12 +14,6 @@ function checkIf(array, num) {
   }
 }
 
-// //.3 function push number in array;
-// function pushIf(condition, number, array) {
-//   if (condition == false) {
-//     array.push(number)
-//   }
-// }
 
 //4. function that check range of a number and isNaN;
 function checkNumber(number, nmin, nmax) {
@@ -45,41 +39,28 @@ console.log(listaBombe);
 
 //2.1 Chiedi all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 var numberListG = [];
-while (numberListG.length != 5) {
+var haiPerso = false;
+while ((numberListG.length != 84) && (haiPerso == false)) {
   var numberG = parseInt(prompt("Inserisci un numero da 1 a 100"));
   var numberGRange = checkNumber(numberG, 1, 100);
-  //2.2 // Controlla che il numero inserito non sia duplicato nell'array utente. Se no pushalo.
+  //2.2 Controlla che il numero inserito non sia duplicato nell'array utente. Se no pushalo.
   if (checkNumG1 = checkIf(numberListG, numberG)) {
     alert("Inserisci un numero che non hai già selezionato.")
   } else {
     numberListG.push(numberG);
     console.log(numberListG);
   }
-  //2.3
+  //2.3 Controlla se il numero inserito dall'utente è presente nella lista bombe; se si termina il ciclo comunicando all'utente il punteggio.
+  var checkNumGBomb = checkIf(listaBombe, numberG);
+  if (checkNumGBomb == true) {
+  alert("Hai perso! Il tuo punteggio è di " + (numberListG.length - 1));
+    haiPerso = true;
+  };
+  if (numberListG.length == 84) {
+    alert("Complimenti! Hai vinto il campominato!")
+  }
 }
 
-
-//2.1 Controlla se il numero inserito dall'utente è presente nella lista delle bombe;
-// var numberListG = [];
-// var checkNumGBomb = check(listaBombe, numberG);
-//
-// while ((numberListG.length != 5) && (checkNumGBomb == false)) {
-//   var numberG;
-//   var checkNumGBomb = check(listaBombe, numberG);
-//   var checkNumG1 = check(numberListG, numberG);
-//   if (check(listaBombe, numberG) == true) {
-//     break;
-//     alert("Bomba");
-//   } else {
-//     if (check(numberListG, numberG) == true) {
-//       alert("Hai inserito un numero già scelto.")
-//     };
-//   numberListG.push(numberG);
-// }
-// }
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // BONUS: (da fare solo se funziona tutto il resto)
 // all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
 // con difficoltà 0 => tra 1 e 100
